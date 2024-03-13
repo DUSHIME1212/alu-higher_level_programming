204007d3480a2d7b961d89e98589493245a25810
Don Aime <118371312+DUSHIME1212@users.noreply.github.com>
2024-03-13 12:06:41 +0200

    2 square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,13 +2,13 @@
 
 """Define a class Square."""
 
+
 class Square:
-    """
-        square with private instance attribute: 'size'
-    """
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
         """
