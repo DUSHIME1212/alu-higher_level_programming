cb293f9e685474ce81ce6e30fff2cc19e117d20a
Don Aime <118371312+DUSHIME1212@users.noreply.github.com>
2024-03-16 09:23:04 +0200

    .

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-# script that adds all arguments to a Python list,
-# and then save them to a file
-"""
-    script that adds all arguments to a Python list,
-    and save them to a file
-"""
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
@@ -19,3 +13,4 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
+    
